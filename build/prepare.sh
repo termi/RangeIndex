fb727ce2b528554d1ac9f6c605410ebea0370f09
termi <termi_uc@inbox.ru>
2014-03-27 21:36:56 +0400

    * prepare script: range-index -> rangeindex

--- build/prepare.sh
+++ build/prepare.sh
@@ -4,19 +4,19 @@ cd ..
 rm -rf build/npm
 mkdir build/npm
 
-git archive master -o build/npm/range-index.tar --prefix=range-index/
+git archive master -o build/npm/rangeindex.tar --prefix=rangeindex/
 
 cd build/npm
 
-tar xf range-index.tar && rm range-index.tar
+tar xf rangeindex.tar && rm rangeindex.tar
 
-cd range-index
+cd rangeindex
 rm .gitignore
 
 cd build
 ./build.sh
 
 cd ../..
-tar czf range-index.tgz range-index && rm -rf range-index
+tar czf rangeindex.tgz rangeindex && rm -rf rangeindex
 
-echo 'tgz file: build/npm/range-index.tgz'
+echo 'tgz file: build/npm/rangeindex.tgz'
